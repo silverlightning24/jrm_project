84dc2ff73e843879ddbdaf10fb34e08fa972e96b
VAIDEHI KRISHNAN <vaidehikrishnan@VAIDEHIs-MacBook-Pro.local>
2019-04-21 13:05:30 +0800

    added user interactions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import Phaser from "phaser";
 import logoImg from "./assets/gingerbreadman.jpg";
+
+
+let gameState = {
+
+}
+
+
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
@@ -7,27 +14,60 @@ const config = {
   height: 600,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update : update,
   }
 };
+
+
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
-  this.load.image("tree","assets/kenney_holidaypack2016/PNG/RTS pack/Default size/RTSobject_03.png")
-  this.load.image("gift","tree","assets/kenney_holidaypack2016/PNG/RTS pack/Default size/RTSobject_03.png")
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-  let tree = this.add.image(100,100,"tree")
-  let gift = this.add.image(500,500,"gift")
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+  
 }
+function create() {
+this.add.text(0,0, "My Adventure In 'Hidden'ville:An amzing experience",{font: "50px impact,bold",fill: "aquamarine", wordWrap: {width: 700,usedAdvancedWrap : true}})
+ 
+gameState["circle"]= this.add.circle(400,400, 90, 0xA75CEE  )
+gameState["square"] = this.add.rectangle(100,105,30,100, 0x0870FE     )
+gameState["circle"].setInteractive()
+gameState["circle"].on("pointerup", function(event){
+  if (this.fillColor === 0xED6769  ){
+    this.fillColor = 0xF0823B
+  } else{
+    this.fillColor = 0x1C98FF
+  }
+  })
+
+gameState["square"].setInteractive
+
+gameState["keyboard"] = this.input.keyboard.createCursorKeys()
+
+
+
+
+
+}
+
+
+
+function update() {
+  if (gameState["keyboard"].left.isDown) {
+    gameState["square"].x -= 10
+  
+  }
+  if (gameState["keyboard"].right.isDown) {
+    gameState["square"].x += 10
+  }
+  if (gameState["keyboard"].down.isDown) {
+    gameState["square"].y += 10
+  
+  }
+  if (gameState["keyboard"].up.isDown) {
+    gameState["square"].y -= 10
+
+}}
+
+
+
