47f980264ef3a23680621946fc85bd07fad9b1a5
VAIDEHI KRISHNAN <vaidehikrishnan@VAIDEHIs-MacBook-Pro.local>
2019-05-12 13:02:28 +0800

    added user interactions and sprite sheets

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -15,6 +15,8 @@ class Boot extends Scene {
         this.load.image("background1", "./src/assets/background.png")
         this.load.image("fireboy", "./src/assets/fireboy.png")
         this.load.image("bat","./src/assets/bat.png")
+        // this.load.image("funnywalk","./src/assets/Walking Sheet.png","./src/assets")
+        this.load.atlasXML("rts","src/assets/kenney_rtssci-fi 2/Spritesheet/scifiRTS_spritesheet.png","src/assets/kenney_rtssci-fi 2/Spritesheet/scifiRTS_spritesheet.xml")
     }
     create() {
         this.scene.start("game")
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -17,10 +17,21 @@ class Game extends Scene {
 
 
 create() {
+    this.cursors = this.input.keyboard.createCursorKeys() ; 
     this.background = this.add.image(0,0,"background1")
     this.background.setOrigin(0,0)
     this.background.setDisplaySize(700,500)
+
+
+    this.char = this.add.sprite(50,50,"rts","scifiUnit_01.png")
+    
+    this.add.sprite(50,50,"rts","scifiUnit_01.png")
+    this.add.sprite(100,100,"rts","scifiUnit_13.png")
+    
+    
     this.spriteGroup = this.add.group()
+    
+    
     for (let i = 0; i < 10; i++) {
         this.spriteGroup.add(new Fireboy({
             scene: this,
@@ -40,6 +51,28 @@ create() {
 }
 
 update(time) {
+    if(this.cursors.left.isDown) {
+        this.char.x -= 5
+    }
+    if(this.cursors.right.isDown) {
+        this.char.x += 5
+    }
+    if(this.cursors.down.isDown) {
+        this.char.y -= 5
+    }
+    if(this.cursors.up.isDown) {
+        this.char.y += 5
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
     let sprites = this.spriteGroup.children.entries
     for(let i=0; i < sprites.length ; i++)
         sprites[i].update(time)
